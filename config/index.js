7ec1ac6e52885f6b2a2e56f4ad14bb5bb85fa52c
Jin <50356049+Dueplay@users.noreply.github.com>
2022-08-31 15:26:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx1609552c1b330e36",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "bd7f97585fd9ed824069a70abd91fb7c",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    TEMPLATE_ID: "lpgxY4_iExceI-O13sAuf3bRo-NCEuOT9OQ0DHt8gjU",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
@@ -25,10 +25,8 @@ export const config = {
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
+      {name: '淇淇', id: "oqYP15ggPnoAd0oHxZwKPHRnSuKk"}, 
+      {name: 'Cookie', id: "oqYP15v4RNocyCeXBPXGCjxG0QNA"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
@@ -42,9 +40,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "重庆",
     // 所在城市
-    CITY: "惠州",
+    CITY: "重庆",
 
     /** 重要节日相关 */
 
@@ -53,10 +51,9 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+      {"type": "生日", "name": "老婆", "year": "2000", "date": "01-12"},
+      {"type": "生日", "name": "我", "year": "2000", "date": "05-23"},
+      {"type": "节日", "name": "在一起纪念日", "year": "2021", "date": "04-16"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
@@ -66,9 +63,8 @@ export const config = {
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
+    LOVE_DATE: "2021-04-16",
     // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
 
 
     /** 每日一言 */
@@ -86,7 +82,6 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
